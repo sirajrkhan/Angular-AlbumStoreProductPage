2bec64faeab4bcf30ce74a24484ed75b862ce8db
Siraj Khan <sirajrkhan@live.in>
2020-06-19 15:52:54 +0530

    mod5

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import ProductDescriptionComponent from '../product-description/product-description.component'
+import { ProductDescriptionComponent } from '../product-description/product-description.component';
+import { ProductTracklistingComponent } from '../product-tracklisting/product-tracklisting.component';
 
 @Component({
   selector: 'app-product-page',
